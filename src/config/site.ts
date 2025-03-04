b235e33f032b945eea0be276b6a4308c8a065d83
Satrio Adi <dev.satrio.ap@gmail.com>
2025-03-05 00:19:13 +0700

    Initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,26 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Caffeine For Breakfast",
+  description: "Your daily dose of scrolls to keep you awake.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  links: {
+    github: "https://github.com/frontio-ai/heroui",
+    instagram: "https://www.instagram.com/setwapres.ri/",
+  },
+};
